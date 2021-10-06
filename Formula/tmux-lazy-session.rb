a4db6d9549e0b15e66300a91d7884381f07b3858
Ingar Almklov <ingara@gmail.com>
2021-10-06 15:41:33 +0200

    Update tmux-lazy-session to v1.0.4

--- Formula/tmux-lazy-session.rb
+++ Formula/tmux-lazy-session.rb
@@ -1,17 +1,10 @@
 class TmuxLazySession < Formula
   desc "Tmux sessions for lazy people"
   homepage "https://github.com/ingara/tmux-lazy-session"
-  url "https://github.com/ingara/tmux-lazy-session.git", tag: "v1.0.3", revision: "3ff1ac1b6ff6b00061fda4c4238d502c815777c4"
+  url "https://github.com/ingara/tmux-lazy-session.git", tag: "v1.0.4", revision: "f58e9c6719edf78a9da138b7e3d76350b04626ae"
   sha256 ""
-  version "v1.0.3"
+  version "v1.0.4"
   license ""
-
-
-  bottle do
-    root_url "https://github.com/ingara/tmux-lazy-session/releases/download/v1.0.3"
-    rebuild 1
-    sha256 cellar: :any, big_sur: ""
-  end
 
   head "https://github.com/ingara/tmux-lazy-session.git", shallow: false
 
